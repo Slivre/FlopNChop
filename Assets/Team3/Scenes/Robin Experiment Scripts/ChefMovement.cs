6631ddebad319de8d107659fdf3d4463695a9aa3
RobinMak4 <101227691+RobinMak4@users.noreply.github.com>
2024-03-06 13:51:33 -0500

    Chef Movement Code
    Chef Movement Code

--- Assets/Team3/Scenes/Robin Experiment Scripts/ChefMovement.cs
+++ Assets/Team3/Scenes/Robin Experiment Scripts/ChefMovement.cs
@@ -13,6 +13,8 @@ namespace team03
 
         Rigidbody rb;
 
+        public Transform fish;
+
         public float HorizontalForce;
 
         private Vector3 dir = new Vector3(0, 0, 1);
@@ -26,10 +28,17 @@ namespace team03
         public bool LPressed;
         public bool RPressed;
 
+        //Code for chef timing
+
+        public float chefCd;
+        public bool chefStart = true;
+
+
 
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
+            chefCd = Random.Range(3f, 5.0f);
 
         }
 
@@ -73,6 +82,32 @@ namespace team03
 
             }
 
+            
+
+            //Timer For Chef Movement
+
+            chefCd -= Time.deltaTime;
+
+            if (chefStart && chefCd < 0)
+            {
+                if (fish.position.z < transform.position.z)
+                {
+                    transform.Translate(dir2 * HorizontalForce * Time.deltaTime);
+                    Debug.Log("right");
+                }
+
+                else if (fish.position.z > transform.position.z)
+                {
+                    transform.Translate(dir * HorizontalForce * Time.deltaTime);
+                    Debug.Log("left");
+                }
+                //else if (fish.position.z == transform.position.z)
+                //{
+                //    chefStart = false; // make sure we dont' call this again }
+
+
+                //}
+            }
         }
 
         protected override void OnButton1Pressed(InputAction.CallbackContext context)
