94b39a70afe1c2c849d335a53f5cacd64e950738
Slivre <zhuguanrui@gmail.com>
2024-03-06 15:41:21 -0500

    Chef Basic Movements

--- Assets/Team3/Scenes/Robin Experiment Scripts/ChefMovement.cs
+++ Assets/Team3/Scenes/Robin Experiment Scripts/ChefMovement.cs
@@ -7,15 +7,15 @@ namespace team03
 {
     public class ChefMovement : MicrogameInputEvents
     {
-        public bool button1Held;
-
-        public float lastButton2Press;
+        public bool HasPlayer;
 
         Rigidbody rb;
 
         public Transform fish;
 
         public float HorizontalForce;
+
+        public float MoveDirection;
 
         private Vector3 dir = new Vector3(0, 0, 1);
         private Vector3 dir2 = new Vector3(0, 0, -1);
@@ -44,70 +44,82 @@ namespace team03
 
         private void Update()
         {
+            if (HasPlayer)
+            {
+                MoveDirection = -stick.normalized.x;
+                Move(MoveDirection);
+            }
+            else
+            {
+                Debug.Log(FollowFish());
+                Move(FollowFish());
+            }
+
+
             //get the objects current position and put it in a variable so we can access it later with less code
             Vector3 pos = transform.position;
+
             //calculate what the new Y position will be
             float newY = Mathf.Sin(Time.time * bobSpeed) * height + pos.y;
             //set the object's Y to the new calculated Y
 
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
 
-            if (LPressed)
-            {
-                transform.Translate(dir * HorizontalForce * Time.deltaTime);
-            }
-            if (RPressed)
-            {
-                transform.Translate(dir2 * HorizontalForce * Time.deltaTime);
-            }
-        
-            if (button1.IsPressed())
-            {
-
-                // Similar to Input.GetButton("Button 1") in the old system.
-
-                button1Held = true;
-           
-            }
-            else
-            {
-
-                button1Held = false;
-
-            }
-
-            if (button2.WasPressedThisFrame())
-            {           
-                lastButton2Press = Time.time;
-
-            }
-
-            
-
+            //if (LPressed)
+            //{
+            //    transform.Translate(dir * HorizontalForce * Time.deltaTime);
+            //}
+            //if (RPressed)
+            //{
+            //    transform.Translate(dir2 * HorizontalForce * Time.deltaTime);
+            //}
             //Timer For Chef Movement
 
             chefCd -= Time.deltaTime;
 
-            if (chefStart && chefCd < 0)
+            //if (chefStart && chefCd < 0)
+            //{
+            //    if (fish.position.z < transform.position.z)
+            //    {
+            //        transform.Translate(dir2 * HorizontalForce * Time.deltaTime);
+            //        Debug.Log("right");
+            //    }
+
+            //    else if (fish.position.z > transform.position.z)
+            //    {
+            //        transform.Translate(dir * HorizontalForce * Time.deltaTime);
+            //        Debug.Log("left");
+            //    }
+            //    //else if (fish.position.z == transform.position.z)
+            //    //{
+            //    //    chefStart = false; // make sure we dont' call this again }
+
+
+            //    //}
+            //}
+        }
+
+        public void Move(float Dir)
+        {           
+            Vector3 HorizontalDir = new Vector3(Dir, 0, 0);
+            Debug.Log(HorizontalDir);
+            transform.Translate(HorizontalDir * HorizontalForce * Time.deltaTime);
+        }
+
+        public float FollowFish()
+        {
+            if (chefStart && chefCd <= 0)
             {
                 if (fish.position.z < transform.position.z)
                 {
-                    transform.Translate(dir2 * HorizontalForce * Time.deltaTime);
-                    Debug.Log("right");
+                    return -1;
                 }
-
-                else if (fish.position.z > transform.position.z)
+                else
                 {
-                    transform.Translate(dir * HorizontalForce * Time.deltaTime);
-                    Debug.Log("left");
+                    return 1;
                 }
-                //else if (fish.position.z == transform.position.z)
-                //{
-                //    chefStart = false; // make sure we dont' call this again }
-
-
-                //}
             }
+            else return 0;
         }
 
         protected override void OnButton1Pressed(InputAction.CallbackContext context)
